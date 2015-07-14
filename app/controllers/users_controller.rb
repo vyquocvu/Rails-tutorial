1fcbde1e0ad7e88d999061388f1837237c741b85
vyquocvu <vyquocvu@gmail.com>
2015-07-14 15:44:06 +0700

    finish c8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      remember user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
       #handle a successful save.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'users/new'
   resources :users
 
@@ -6,6 +7,12 @@ Rails.application.routes.draw do
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
   get 'signup'  =>  'users#new'
+  
+  get 'sessions/new'
+  get 'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
